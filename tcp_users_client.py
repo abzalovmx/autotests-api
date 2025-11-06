ae296bbc5034989047099b36a2880f866172ce4a
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-11-06 15:08:31 +0500

    Практика работы с TCP/IP протоколом

---
+++ tcp_users_client.py
@@ -0,0 +1,13 @@
+import socket
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+message = "Привет, сервер!"
+client_socket.send(message.encode())
+
+response = client_socket.recv(1024).decode()
+print(response)
+
+client_socket.close()
