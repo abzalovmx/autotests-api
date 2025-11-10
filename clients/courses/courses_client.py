a8ff32eedbea4c05b2aa9d9466b83e0b76db5d83
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-11-10 16:59:41 +0500

    6.5 Практикуемся в использовании API клиентов

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,87 +1,99 @@
 from typing import TypedDict
+
 from httpx import Response
+
 from clients.api_client import APIClient
+from clients.privet_http_builder import get_private_http_client, AuthenticationUserDict
 
 
-class GetExercisesQueryDict(TypedDict):
+class GetCoursesQueryDict(TypedDict):
     """
-    Описание структуры запроса на получение списка заданий.
+    Описание структуры запроса на получение списка курсов.
     """
-    courseId: str
+    userId: str
 
 
-class CreateExercisesRequestDict(TypedDict):
+class CreateCourseRequestDict(TypedDict):
     """
-    Описание структуры запроса на создание задания.
-    """
-    title: str
-    courseId: str
-    maxScore: int
-    minScore: int
-    orderIndex: int
-    description: str
-    estimatedTime: str
-
-
-class UpdateExercisesRequestDict(TypedDict):
-    """
-    Описание структуры запроса на обновление задания.
+    Описание структуры запроса на создание курса.
     """
     title: str
     maxScore: int
     minScore: int
     description: str
     estimatedTime: str
+    previewFileId: str
+    createdByUserId: str
 
 
-class ExercisesClient(APIClient):
+class UpdateCourseRequestDict(TypedDict):
     """
-    Клиент для работы с /api/v1/exercises
+    Описание структуры запроса на обновление курса.
+    """
+    title: str | None
+    maxScore: int | None
+    minScore: int | None
+    description: str | None
+    estimatedTime: str | None
+
+
+class CoursesClient(APIClient):
+    """
+    Клиент для работы с /api/v1/courses
     """
 
-    def get_exercises_api(self, query: GetExercisesQueryDict) -> Response:
+    def get_courses_api(self, query: GetCoursesQueryDict) -> Response:
         """
-        Метод получения списка заданий.
+        Метод получения списка курсов.
 
-        :param query: Словарь с courseId.
+        :param query: Словарь с userId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/exercises", params=query)
+        return self.get("/api/v1/courses", params=query)
 
-    def get_exercise_api(self, exercise_id: str) -> Response:
+    def get_course_api(self, course_id: str) -> Response:
         """
-        Метод получения задания.
+        Метод получения курса.
 
-        :param exercise_id: Идентификатор задания.
+        :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/exercises/{exercise_id}")
+        return self.get(f"/api/v1/courses/{course_id}")
 
-    def create_exercise_api(self, request: CreateExercisesRequestDict) -> Response:
+    def create_course_api(self, request: CreateCourseRequestDict) -> Response:
         """
-        Метод создания задания.
+        Метод создания курса.
 
-        :param request: Словарь с title, courseId, maxScore, minScore, orderIndex,
-        description, estimatedTime.
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+        previewFileId, createdByUserId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/exercises", json=request)
+        return self.post("/api/v1/courses", json=request)
 
-    def update_exercise_api(self, exercise_id: str, request: UpdateExercisesRequestDict) -> Response:
+    def update_course_api(self, course_id: str, request: UpdateCourseRequestDict) -> Response:
         """
-        Метод обновления задания.
+        Метод обновления курса.
 
-        :param exercise_id: Идентификатор задания.
+        :param course_id: Идентификатор курса.
         :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/exercises/{exercise_id}", json=request)
+        return self.patch(f"/api/v1/courses/{course_id}", json=request)
 
-    def delete_exercise_api(self, exercise_id: str) -> Response:
+    def delete_course_api(self, course_id: str) -> Response:
         """
-        Метод удаления задания.
+        Метод удаления курса.
 
-        :param exercise_id: Идентификатор задания.
+        :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/exercises/{exercise_id}")
+        return self.delete(f"/api/v1/courses/{course_id}")
+
+
+def get_courses_client(user: AuthenticationUserDict) -> CoursesClient:
+    """
+    Функция создаёт экземпляр CoursesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию CoursesClient.
+    """
+    return CoursesClient(client=get_private_http_client(user))
