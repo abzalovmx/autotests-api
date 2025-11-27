95bad8ef8ee35dafff5333c43a6a61d490dc00ce
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-11-27 20:04:16 +0500

    Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from pydantic import BaseModel, EmailStr
 from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
+from clients.privet_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
@@ -17,6 +19,10 @@ class UserFixture(BaseModel):
     def password(self) -> str:
         return self.request.password
 
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:
+        return AuthenticationUserSchema(email=self.email, password=self.password)
+
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
@@ -32,4 +38,10 @@ def public_users_client() -> PublicUsersClient:
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
+
     return UserFixture(request=request, response=response)
+
+
+@pytest.fixture
+def private_users_client(function_user) -> PrivateUsersClient:
+    return get_private_users_client(function_user.authentication_user)
