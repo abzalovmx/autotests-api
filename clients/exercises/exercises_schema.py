9375e4f55d82e8b4b4bee98156ccaae98997b419
abzalovmx <abzalov484@gmail.com>
2025-12-08 19:11:02 +0500

    Практика написания API автотестов для работы с заданиями. Обновление задания

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -114,3 +114,26 @@ class GetExerciseResponseSchema(BaseModel):
     """
 
     exercise: ExerciseSchema
+
+
+class UpdateExerciseRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на обновление задания.
+    """
+
+    model_config = ConfigDict(populate_by_name=True)
+
+    title: str | None = Field(default_factory=fake.sentence)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str | None = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class UpdateExerciseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на обновление задания.
+    """
+
+    exercise: ExerciseSchema
