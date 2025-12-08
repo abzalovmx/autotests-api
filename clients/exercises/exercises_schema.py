042189675acba405e8697f4afdc1fd4b5d4e23de
abzalovmx <abzalov484@gmail.com>
2025-12-08 18:47:29 +0500

    Практика написания API автотестов для работы с заданиями. Создание задания

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -82,3 +82,27 @@ class UpdateExercisesRequestSchema(BaseModel):
     min_score: int = Field(alias="minScore", default_factory=fake.min_score)
     description: str = Field(default_factory=fake.text)
     estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class CreateExerciseRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание задания.
+    """
+
+    model_config = ConfigDict(populate_by_name=True)
+
+    title: str = Field(default_factory=fake.sentence)
+    course_id: str = Field(alias="courseId", default_factory=fake.uuid4)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class CreateExerciseResponsetSchema(BaseModel):
+    """
+    Описание структуры ответа создания задания.
+    """
+
+    exercise: ExerciseSchema
